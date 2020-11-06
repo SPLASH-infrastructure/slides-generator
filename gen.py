eb7c0285b4c4f3b735518ad038f2d8c3d9ff7853
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2020-11-06 12:04:42 +1100

    Update layout & styles

--- gen.py
+++ gen.py
@@ -11,12 +11,11 @@ SCHEDULE_XML = './data/splash-schedule.xml'
 
 xml = scheduleloader.loadXML(SCHEDULE_XML)
 
-def generateSubEvent(subevent_id):
+def generateSubEvent(subevent_id, image_only=False):
     events = scheduleloader.parseSubEvent(xml, subevent_id)
     for i in range(len(events)):
         e = events[i]
-        next_e = events[i + 1] if i < len(events) - 1 else None
-        videogen.generateVideoFromEvent(event=e, duration=(INTRO_LENGTH, QA_LENGTH, EXIT_LENGTH), next_event=next_e)
+        videogen.generateVideoFromEvent(event=e, duration=(INTRO_LENGTH, QA_LENGTH, EXIT_LENGTH), image_only=image_only)
 
 def printHelp():
     print('Usage:')
@@ -28,7 +27,7 @@ def printHelp():
     print('    python3 ./gen.py --subevent=c49b3977-bf78-4796-930d-b360d8899600')
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], 'h', [ 'subevent=' ])
+    opts, args = getopt.getopt(sys.argv[1:], 'h', [ 'subevent=', 'imageonly' ])
 except getopt.GetoptError:
     printHelp()
     sys.exit(2)
@@ -38,9 +37,15 @@ for opt, arg in opts:
         printHelp()
         sys.exit()
 
+image_only = False
+
+for opt, arg in opts:
+    if opt == '--imageonly':
+        image_only = True
+
 for opt, arg in opts:
     if opt == '--subevent':
-        generateSubEvent(arg)
+        generateSubEvent(arg, image_only=image_only)
         sys.exit()
 
 for subevent in scheduleloader.getAllSubEventIds(xml):
--- videogen.py
+++ videogen.py
@@ -1,34 +1,35 @@
 import tempfile
 import os
-from jinja2 import Template
+from jinja2 import FileSystemLoader, Environment
 from scheduleloader import ConferenceEvent
 
 
 
 def htmlToPNG(html, png, env):
-    with open(html, 'r') as template_file:
-        with open('./slides/.temp.html', 'w') as out_file:
-            template = Template(template_file.read())
-            out_file.write(template.render(**env))
-        os.system(f'wkhtmltoimage --enable-local-file-access --disable-smart-width --height 1080 --width 1960 ./slides/.temp.html {png}')
+    templateLoader = FileSystemLoader(searchpath=os.path.dirname(html))
+    templateEnv = Environment(loader=templateLoader)
+    template = templateEnv.get_template(os.path.basename(html))
+    with open('./slides/.temp.html', 'w') as out_file:
+        out_file.write(template.render(**env))
+    os.system(f'wkhtmltoimage --enable-local-file-access --disable-smart-width --height 1080 --width 1960 ./slides/.temp.html {png}')
     return png
 
 def stillImageToVideo(image, video, duration):
     os.system(f'ffmpeg -y -loop 1 -framerate 30 -i {image} -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 -af "aresample=async=1:min_hard_comp=0.100000:first_pts=0" -g 60 -c:v libx264 -r 30 -t {duration} -pix_fmt yuv420p {video}')
 
 # Generate videos from a timeslot
-def generateVideoFromEvent(event: ConferenceEvent, duration, next_event=None):
+def generateVideoFromEvent(event: ConferenceEvent, duration, image_only=False):
     out = f'./out/{event.conference.subevent_id}/{event.event_id}'
     os.system(f'mkdir -p {out}')
     # Intro
     image = f'{out}/intro-000.png'
-    htmlToPNG('./slides/intro-template.html', image, { 'event': event, 'time': '00:00', 'next_event': next_event })
-    stillImageToVideo(image=image, video=f'{out}/intro.mp4', duration=duration[0])
+    htmlToPNG('./slides/intro-template.html', image, { 'event': event, 'time': '00:00' })
+    if not image_only: stillImageToVideo(image=image, video=f'{out}/intro.mp4', duration=duration[0])
     # Q & A
     image = f'{out}/qa-000.png'
-    htmlToPNG('./slides/qa-template.html', image, { 'event': event, 'time': '00:00', 'next_event': next_event })
-    stillImageToVideo(image=image, video=f'{out}/qa.mp4', duration=duration[1])
+    htmlToPNG('./slides/qa-template.html', image, { 'event': event, 'time': '00:00' })
+    if not image_only: stillImageToVideo(image=image, video=f'{out}/qa.mp4', duration=duration[1])
     # Exit
     image = f'{out}/exit-000.png'
-    htmlToPNG('./slides/exit-template.html', image, { 'event': event, 'time': '00:00', 'next_event': next_event })
-    stillImageToVideo(image=image, video=f'{out}/exit.mp4', duration=duration[2])
+    htmlToPNG('./slides/exit-template.html', image, { 'event': event, 'time': '00:00' })
+    if not image_only: stillImageToVideo(image=image, video=f'{out}/exit.mp4', duration=duration[2])
