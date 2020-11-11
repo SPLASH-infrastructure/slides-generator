a507efd229e2ffc1d0b3d1448c8f4ddd2c51fd28
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2020-11-12 09:02:56 +1100

    Generate videos only for a time range

--- gen-events.py
+++ gen-events.py
@@ -2,12 +2,15 @@
 
 import splash
 import argparse
+from datetime import datetime
 
 
 parser = argparse.ArgumentParser(description='Generate all event splash videos')
 
 parser.add_argument('--stream', type=str, help='Generate splash videos only for the given stream. Must oe one of ["OOPSLA", "Rebase", "SPLASH"].')
 parser.add_argument('--event', type=str, help='Generate splash videos only for the given event-id. (--stream must be specified)')
+parser.add_argument('--start', type=str, help='Time range limits')
+parser.add_argument('--end', type=str, help='Time range limits')
 
 args = parser.parse_args()
 
@@ -20,10 +23,19 @@ print(len(events))
 
 if args.stream is not None:
     # Generate videos for the given stream
-    events = [ e for e in events if e.stream.name == args.stream ]
+    events = [ e for e in events if e.stream.stream_id == args.stream ]
 if args.event is not None:
     # Generate videos for the given event id
     events = [ e for e in events if e.event_id == args.event ]
+if args.start is not None:
+    start = datetime.strptime(args.start, '%y-%m-%d-%H:%M')
+    # Generate videos for the given event id
+    events = [ e for e in events if e.start.time >= start ]
+if args.end is not None:
+    end = datetime.strptime(args.end, '%y-%m-%d-%H:%M')
+    # for e in events: print(e.start.time)
+    # Generate videos for the given event id
+    events = [ e for e in events if e.end.time <= end ]
 
 # Generate
 # print(events)
