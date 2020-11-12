1d1f86de9cbee9f5c62927f69f67f0f6e2d3b7c7
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2020-11-12 15:28:29 +1100

    Update blank slides

--- gen-blank.py
+++ gen-blank.py
@@ -8,3 +8,8 @@ from splash.keyframe import KeyFrame
 frame = KeyFrame.render_from_template('./slides/blank.html', './out/blank.png', 5, {})
 
 splash.video.generateFromKeyFrames([frame], './out/blank.mp4')
+
+
+frame = KeyFrame.render_from_template('./slides/blank-transition.html', './out/blank-transition.png', 5, {})
+
+splash.video.generateFromKeyFrames([frame], './out/blank-transition.mp4')
